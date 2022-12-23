5a7e1412ff76c3169fd36d853c22cd45874a7ffb
백대홍 <bdh3620@naver.com>
2022-12-23 23:08:48 +0900

    Complete

--- PetGallary/DetailViewController.swift
+++ PetGallary/DetailViewController.swift
@@ -14,9 +14,23 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never  // 세부정보 페이지의 선택한 제목 텍스트 크기 설정
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {  // 상단바 보이게 하기
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+        
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {  //상단바 숨기기
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
     
 
--- PetGallary/ViewController.swift
+++ PetGallary/ViewController.swift
@@ -9,27 +9,26 @@ import UIKit
 
 class ViewController: UITableViewController {
     var pictures = [String]()
+    let sections = ["Geko", "Dog", "Cat"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Pet Gallary"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let path  = "/Users/baegdaehong/Desktop/xcode/PetGallary/Pic"
         let filemanager = FileManager.default
         
         let items = try! filemanager.contentsOfDirectory(atPath: path)
         
-        for item in items {
+        for item in items.sorted() {            // 사진을 들고올때 정렬을 해서 들고오세요
             if item.hasPrefix("Pet") {         // 여기서 사진을 읽어 옴
                 pictures.append(item)
             }
         }
-        print(pictures)
     }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pictures.count
-    }
-    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { // 테이블뷰에 셀로 사진 텍스트 출력
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
