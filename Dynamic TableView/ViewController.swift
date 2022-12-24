6b7453d3c78d55e70ecd122c9a097778eec79237
백대홍 <bdh3620@naver.com>
2022-12-24 16:44:56 +0900

    Dynamic TableView

---
+++ Dynamic TableView/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Dynamic TableView
+//
+//  Created by 백대홍 on 2022/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    let contentArray = ["A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with"]
+    
+    
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        // cell 리소스 파일 가져오기
+        let myTableViewCellNib = UINib(nibName: String(describing: MyTableViewCell.self), bundle: nil)
+        
+        // cell에 리소스 등록
+        self.myTableView.register(myTableViewCellNib, forCellReuseIdentifier: "myTableViewCell")
+        
+        self.myTableView.rowHeight = UITableView.automaticDimension
+        self.myTableView.estimatedRowHeight = 120
+        
+        
+        // 베리 중요
+        self.myTableView.delegate = self
+        self.myTableView.dataSource = self
+        
+        
+        
+    }
+    
+    //테이블 뷰 cell의 개수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    
+    //
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+
+}
+
