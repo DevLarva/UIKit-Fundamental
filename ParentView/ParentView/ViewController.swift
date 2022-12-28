d567064e792a68a22fc2981ddadec1874cfcb8c1
백대홍 <bdh3620@naver.com>
2022-12-28 18:14:13 +0900

    parentview

---
+++ ParentView/ParentView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParentView
+//
+//  Created by 백대홍 on 2022/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
