b4871ec3af4fabbe56dd7e7ac50aa78d7dfc6345
백대홍 <bdh3620@naver.com>
2023-01-02 23:40:51 +0900

    gesture Recognizer

--- Target-Action-Date Picker/Target-Action-Date Picker/ViewController.swift
+++ Target-Action-Date Picker/Target-Action-Date Picker/ViewController.swift
@@ -12,16 +12,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     
     
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {      //액션 메서드 연결 및 호출
+        self.view.endEditing(true)
+        print("tap")
+        
+}
+    
     let dateFormatter: DateFormatter = {                        //데이터 포멧
         let formatter: DateFormatter = DateFormatter()
-//        formatter.dateStyle = .full   // 날짜
-//        formatter.timeStyle = .long   // 시간
+       formatter.dateStyle = .full   // 날짜
+        formatter.timeStyle = .long   // 시간
         formatter.dateFormat = "yyyy/MM/dd hh:mm:ss"                   //위 2개처럼 쓸수도 있고 해당 줄처럼 쓸 수도 있음
         return formatter
     }()
      
     
-    
+
     
     @IBAction func didDatePickerValueChanged(_ sender: UIDatePicker) {  //데이터 피커에서 값 변하면 출력
         print("value changed")
@@ -37,11 +43,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.datePicker.addTarget(self, action: #selector(self.didDatePickerValueChanged(_:)), for: UIControl.Event.valueChanged)
+        
+//        self.datePicker.addTarget(self, action: #selector(self.didDatePickerValueChanged(_:)), for: UIControl.Event.valueChanged)   //datepicker
+
+        //액션 타깃을 통한 제스처 인식기 초기화 및 생성
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.tapView(_:)))
+        
+        // 뷰에 제스처 인식기 연결
+        self.view.addGestureRecognizer(tapRecognizer)
     }
+      
     
-    
-
-
+ 
 }
 
