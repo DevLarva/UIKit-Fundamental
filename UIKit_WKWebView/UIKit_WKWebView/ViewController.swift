08d582e4da824fb347cfa6092694b4c6c9b7e1f3
백대홍 <bdh3620@naver.com>
2023-11-06 23:43:55 +0900

    feat: WKWebView 실습

--- UIKit_WKWebView/UIKit_WKWebView/ViewController.swift
+++ UIKit_WKWebView/UIKit_WKWebView/ViewController.swift
@@ -6,12 +6,23 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!     ///WKWebView 인스턴스를 저장할 webview 변수 선언
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self   ///webView의 navigationDelegate를 현재 뷰 컨트롤러로 설정합니다.
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!  ///URL 객체 생성
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true  ///webview에서 앞, 뒤로 이동하는 제스처 허용
     }
 
 
