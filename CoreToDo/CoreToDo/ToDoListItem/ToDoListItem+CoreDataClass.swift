8d3b44a4cee371b755ec5a2fa78869c2ea68fd91
백대홍 <bdh3620@naver.com>
2023-11-23 14:44:48 +0900

    feat Core Data TodoList app add

---
+++ CoreToDo/CoreToDo/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreToDo
+//
+//  Created by 백대홍 on 11/23/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
