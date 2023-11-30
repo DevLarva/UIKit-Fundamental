6fccfca63a07bdfd523b4f412746fb565fc0e177
백대홍 <bdh3620@naver.com>
2023-11-30 23:57:18 +0900

    feat: Auth Controller edit(clientID)

--- Spotify_UIKit/Spotify_UIKit/Managers/AuthManager.swift
+++ Spotify_UIKit/Spotify_UIKit/Managers/AuthManager.swift
@@ -11,6 +11,11 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "b78b9eec7cfb4b9e904d3e2f63103e58"
+        static let clientSecret = "a8b366d65aff4a469823e5d2748af4f4"
+    }
+    
     private init() {}
     
     var isSignedIn: Bool {
